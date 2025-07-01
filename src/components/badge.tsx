9eaf334054d49bf000447982742e984a65f15bd0
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-07-01 20:43:17 -0300

    Add ellipsis

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -8,11 +8,11 @@ interface BadgeProps {
 
 export function Badge(props: BadgeProps) {
 	return (
-		<div className='rounded-full bg-white py-2 px-3 border border-[#E2DCE7] h-9 flex items-center gap-1 w-fit'>
+		<div className='rounded-full bg-white py-2 px-3 border border-[#E2DCE7] h-9 flex items-center gap-1 max-w-full'>
 			{props.icon}
-			<div className='flex gap-1'>
-				<span className='text-sm text-[#6727A6] leading-5'>{props.title}:</span>
-				<span className='text-[16px] text-black leading-5'>{props.value}</span>
+			<div className='flex gap-1 min-w-0 flex-1'>
+				<span className='text-sm text-[#6727A6] leading-5 whitespace-nowrap'>{props.title}:</span>
+				<span className='text-[16px] text-black leading-5 truncate'>{props.value}</span>
 			</div>
 		</div>
 	)
