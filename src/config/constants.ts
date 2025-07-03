c4aac1c18fb8f08c2c9b8da91e236dfe856084fb
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-07-03 11:07:59 -0300

    Extract tosat duration into a constant

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,9 @@
 export const IGDB_API_URL = 'https://api.igdb.com/v4/games'
 export const IGDB_IMAGE_BASE_URL = 'https://images.igdb.com/igdb/image/upload'
 export const LOCAL_STORAGE_COLLECTED_GAMES_KEY = 'collected_games'
+
+/**
+ * Duration in milliseconds for toast notifications and
+ * temporary button disable state after collecting/uncollecting a game
+ */
+export const TOAST_DURATION = 3000
