329066a0e3d7959e4f9e834ea6df7c3e1569dbcc
Martin <59508264+martinc1991@users.noreply.github.com>
2025-07-03 15:21:45 -0300

    Single source for typography (#11)
    * Single source for typography
    
    * Apply classnames

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { Title } from '@/components/typography/title'
+import { Typography } from '@/components/typography'
 import { Button } from '@/components/ui/button'
 
 export default function ErrorPage({ reset }: { reset: () => void }) {
 	return (
 		<div className='flex flex-col items-center space-y-6 gap-6 mt-10'>
-			<Title>Something went wrong.</Title>
+			<Typography.H1>Something went wrong.</Typography.H1>
 			<div className='flex gap-4'>
 				<Button className='bg-main hover:bg-main/60 hover:cursor-pointer' onClick={reset}>
 					Try again
---
+++ src/components/typography/index.tsx
@@ -0,0 +1,40 @@
+import { cn } from '@/lib/utils'
+import { HTMLAttributes } from 'react'
+
+const gradientClass = 'bg-gradient-to-r from-[#3C1661] to-[#6727A6] bg-clip-text text-transparent'
+
+// Headings
+export function H1({ className, ...props }: HTMLAttributes<HTMLHeadingElement>) {
+	return <h1 className={cn(gradientClass, 'text-[20px] font-[700] sm:text-[24px]', className)} {...props} />
+}
+
+export function H2({ className, ...props }: HTMLAttributes<HTMLHeadingElement>) {
+	return <h2 className={cn(gradientClass, 'text-[18px] font-semibold leading-5', className)} {...props} />
+}
+
+export function H3({ className, ...props }: HTMLAttributes<HTMLHeadingElement>) {
+	return <h3 className={cn(gradientClass, 'text-[16px] font-semibold', className)} {...props} />
+}
+
+// Styled
+export function Highlighted({ className, ...props }: HTMLAttributes<HTMLSpanElement>) {
+	return <span className={cn(gradientClass, className)} {...props} />
+}
+
+export function Muted({ className, ...props }: HTMLAttributes<HTMLSpanElement>) {
+	return <span className={cn('text-gray-500', className)} {...props} />
+}
+
+// Paragraph
+export function P({ className, ...props }: HTMLAttributes<HTMLParagraphElement>) {
+	return <p className={cn('text-[16px] leading-5 text-gray-900', className)} {...props} />
+}
+
+export const Typography = {
+	H1,
+	H2,
+	H3,
+	Highlighted,
+	Muted,
+	P,
+}
